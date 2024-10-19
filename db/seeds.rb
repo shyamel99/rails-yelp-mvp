caa9f51fe5730815203b6816f00fa01f97c38318
vidoor mathoora <shyamalmathoora@gmail.com>
2024-10-19 10:19:03 +0400

    50% completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category:"french", phone_number: "971345673456"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "chinese", phone_number:"845837456"}
+pizzahut = {name: "Pizza Hut", address: "70A ditch High St, London E1 6PQ", category: "chinese",  phone_number:"795132454"}
+crumble = {name: "Crumble", address: "5B oredit Low St, Cape Town ", category:"italian", phone_number:"72139465734650"}
+lecker = {name: "Lecker", address: "23 Shotch medium Str., Berlin", category:"chinese",  phone_number:"1284956034561389"}
+
+
+[dishoom, pizza_east, pizzahut, crumble, lecker].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
